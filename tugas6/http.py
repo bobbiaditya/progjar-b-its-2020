57d6be6d265e34089fa26e6e587204bb79d266b1
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-12 09:34:59 +0700

    up

--- tugas6/http.py
+++ tugas6/http.py
@@ -7,11 +7,7 @@ from datetime import datetime
 class HttpServer:
 	def __init__(self):
 		self.sessions={}
-		self.types={}
-		self.types['.pdf']='application/pdf'
-		self.types['.jpg']='image/jpeg'
-		self.types['.txt']='text/plain'
-		self.types['.html']='text/html'
+
 	def response(self,kode=404,message='Not Found',messagebody='',headers={}):
 		tanggal = datetime.now().strftime('%c')
 		resp=[]
@@ -40,7 +36,7 @@ class HttpServer:
 			method=j[0].upper().strip()
 			if (method=='GET'):
 				object_address = j[1].strip()
-				object_address = object_address.replace('/', '')
+				# object_address = object_address.replace('/', '')
 				print(object_address)
 				return self.http_get(object_address)
 			else:
@@ -48,22 +44,21 @@ class HttpServer:
 		except IndexError:
 			return self.response(400,'Bad Request','',{})
 	def http_get(self,object_address):
-		files = glob('./*')
-		thedir = '.\\'
-		print(thedir+object_address)
-		print(files)
-		if thedir+object_address not in files:
-			return self.response(404,'Not Found','',{})
-		fp = open(thedir+object_address,'r')
-		isi = fp.read()
-
-		fext = os.path.splitext(thedir+object_address)[1]
-		content_type = self.types[fext]
-
-		headers={}
-		headers['Content-type']=content_type
-
-		return self.response(200,'OK',isi,headers)
+		# files = glob('./*')
+		# thedir = '.\\'
+		# print(thedir+object_address)
+		# print(files)
+		# if thedir+object_address not in files:
+		# 	return self.response(404,'Not Found','',{})
+		# fp = open(thedir+object_address,'r')
+		# isi = fp.read()
+		#
+		# fext = os.path.splitext(thedir+object_address)[1]
+		# content_type = self.types[fext]
+		#
+		# headers={}
+		# headers['Content-type']=content_type
+		return self.response(200,'OK','<h1>SERVER HTTP</h1>',{})
 		
 			 	
 #>>> import os.path
