1b4b487a8964f51ca0d9d4380e67ce5a1a654246
bobbiaditya <bobbi.aditya@gmail.com>
2020-04-24 13:10:02 +0700

    done

---
+++ tugas8/README.md
@@ -0,0 +1,11 @@
+# Tugas 8
+
+## Tampilan page awal localhost:10002/sending.html
+![1](images/tampilan_awal.png)
+
+## Mengubah isi form dengan 'test'
+![1](images/tampilan_akhir.png)
+
+## Balasan dari server
+![1](images/hasil_akhir.png)
+
--- tugas8/http.py
+++ tugas8/http.py
@@ -12,6 +12,7 @@ class HttpServer:
 		self.types['.jpg']='image/jpeg'
 		self.types['.txt']='text/plain'
 		self.types['.html']='text/html'
+
 	def response(self,kode=404,message='Not Found',messagebody='',headers={}):
 		tanggal = datetime.now().strftime('%c')
 		resp=[]
@@ -30,47 +31,50 @@ class HttpServer:
 		return response_str
 
 	def proses(self,data):
-		
 		requests = data.split("\r\n")
-		#print(requests)
-
+		# print(requests)
 		baris = requests[0]
-		#print(baris)
-
 		all_headers = [n for n in requests[1:] if n!='']
-
 		j = baris.split(" ")
 		try:
 			method=j[0].upper().strip()
+			# print(method)
 			if (method=='GET'):
 				object_address = j[1].strip()
+				object_address = object_address.replace('/', '')
+				print(object_address)
 				return self.http_get(object_address, all_headers)
 			if (method=='POST'):
+				temp = requests[18].rsplit("=")
+				# print(temp)
+				form = temp[1]
+				print(form)
 				object_address = j[1].strip()
-				return self.http_post(object_address, all_headers)
+				return self.http_post(object_address, all_headers, form)
 			else:
 				return self.response(400,'Bad Request','',{})
 		except IndexError:
 			return self.response(400,'Bad Request','',{})
+
 	def http_get(self,object_address,headers):
 		files = glob('./*')
-		thedir='.'
+		thedir='.\\'
 		if thedir+object_address not in files:
 			return self.response(404,'Not Found','',{})
 		fp = open(thedir+object_address,'r')
 		isi = fp.read()
-		
-		fext = os.path.splitext(thedir+object_address)[1]
-		content_type = self.types[fext]
-		
 		headers={}
-		headers['Content-type']=content_type
-		
+		headers['Content-type']="text/html"
 		return self.response(200,'OK',isi,headers)
-	def http_post(self,object_address,headers):
-		headers ={}
-		isi = "kosong"
-		return self.response(200,'OK',isi,headers)
+
+	def http_post(self, object_address, headers, form):
+		head = headers
+		headers = {}
+		temp = ""
+		for h in head:
+			temp = temp + h + "\n"
+		isi = form + "\n\n" + temp
+		return self.response(200, 'OK', isi, headers)
 		
 			 	
 #>>> import os.path
@@ -80,10 +84,10 @@ if __name__=="__main__":
 	httpserver = HttpServer()
 	d = httpserver.proses('GET testing.txt HTTP/1.0')
 	print(d)
-	d = httpserver.http_get('testing2.txt')
-	print(d)
-	d = httpserver.http_get('testing.txt')
-	print(d)
+	# d = httpserver.http_get('testing2.txt')
+	# print(d)
+	# d = httpserver.http_get('testing.txt')
+	# print(d)
 
 
 
